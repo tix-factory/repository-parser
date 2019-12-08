9346df2960f18893693be33bb3bef9c5c69cabd9
WebGL3D <OgreLandBob@gmail.com>
2019-12-08 15:55:13 -0800

    Add DependencyGrapher

---
+++ TixFactory.RepositoryParser/TixFactory.RepositoryParser/Implementation/DependencyGrapher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TixFactory.RepositoryParser
+{
+	/// <inheritdoc cref="IDependencyGrapher"/>
+	public class DependencyGrapher : IDependencyGrapher
+	{
+		/// <inheritdoc cref="IDependencyGrapher.ParseDependencyTiers"/>
+		public IReadOnlyCollection<IReadOnlyCollection<IProject>> ParseDependencyTiers(IReadOnlyCollection<IProject> allProjects)
+		{
+			var dependencyTiers = new HashSet<IReadOnlyCollection<IProject>>();
+			var remainingProjects = allProjects.ToList();
+			
+			while (remainingProjects.Any())
+			{
+				var tier = new HashSet<IProject>();
+
+				foreach (var project in remainingProjects)
+				{
+					var canBuild = true;
+					foreach (var dependentProject in project.ProjectDependencies)
+					{
+						if (remainingProjects.Contains(dependentProject))
+						{
+							canBuild = false;
+							break;
+						}
+					}
+
+					if (canBuild)
+					{
+						tier.Add(project);
+					}
+				}
+
+				foreach (var project in tier)
+				{
+					remainingProjects.Remove(project);
+				}
+
+				dependencyTiers.Add(tier);
+			}
+
+			return dependencyTiers;
+		}
+	}
+}
---
+++ TixFactory.RepositoryParser/TixFactory.RepositoryParser/Interfaces/IDependencyGrapher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TixFactory.RepositoryParser
+{
+	/// <summary>
+	/// For parsing project dependency graphs.
+	/// </summary>
+	public interface IDependencyGrapher
+	{
+		/// <summary>
+		/// Parses all projects into a collection of collections based on dependencies.
+		/// </summary>
+		/// <remarks>
+		/// Each collection can be built in parallel, the returned list of collections must be built in order.
+		/// </remarks>
+		/// <returns>A tiered collection of <see cref="IProject"/> collections.</returns>
+		IReadOnlyCollection<IReadOnlyCollection<IProject>> ParseDependencyTiers(IReadOnlyCollection<IProject> allProjects);
+	}
+}
