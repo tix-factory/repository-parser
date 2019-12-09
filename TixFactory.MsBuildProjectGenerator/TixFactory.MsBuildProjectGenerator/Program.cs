4c0388cb161324ec14352b5d53843a562202845a
WebGL3D <OgreLandBob@gmail.com>
2019-12-08 17:27:30 -0800

    Program class code tidiness

--- TixFactory.MsBuildProjectGenerator/TixFactory.MsBuildProjectGenerator/Program.cs
+++ TixFactory.MsBuildProjectGenerator/TixFactory.MsBuildProjectGenerator/Program.cs
@@ -4,8 +4,31 @@ using TixFactory.RepositoryParser;
 
 namespace TixFactory.MsBuildProjectGenerator
 {
+	/// <summary>
+	/// The application entry class.
+	/// </summary>
 	public class Program
 	{
+		private readonly IRepositoryParser _RepositoryParser;
+		private readonly IProjectBuilder _ProjectBuilder;
+
+		/// <summary>
+		/// Initializes a new <see cref="Program"/>.
+		/// </summary>
+		public Program()
+		{
+			var repositoryParser = new RepositoryParser.RepositoryParser();
+			var dependencyGrapher = new DependencyGrapher();
+			var projectBuilder = new ProjectBuilder(dependencyGrapher);
+
+			_RepositoryParser = repositoryParser;
+			_ProjectBuilder = projectBuilder;
+		}
+
+		/// <summary>
+		/// The application entry method.
+		/// </summary>
+		/// <param name="args">The application start arguments.</param>
 		public static void Main(string[] args)
 		{
 			if (args.Length != 2)
@@ -14,28 +37,35 @@ namespace TixFactory.MsBuildProjectGenerator
 				Environment.Exit(1);
 				return;
 			}
+			
+			var program = new Program();
+			program.Run(args[0], args[1]);
+		}
 
-			var inputDirectory = args[0];
+		/// <summary>
+		/// Runs the application.
+		/// </summary>
+		/// <param name="inputDirectory">The input directory to parse.</param>
+		/// <param name="outputFilePath">The output file path for the parallel build project.</param>
+		/// <exception cref="DirectoryNotFoundException">
+		/// - <paramref name="inputDirectory"/> does not exist.
+		/// - <paramref name="outputFilePath"/> is in directory that does not exist.
+		/// </exception>
+		public void Run(string inputDirectory, string outputFilePath)
+		{
 			if (!Directory.Exists(inputDirectory))
 			{
 				throw new DirectoryNotFoundException($"'{nameof(inputDirectory)}' does not exist.");
 			}
 
-			var outputFilePath = args[1];
 			if (!Directory.Exists(Path.GetDirectoryName(outputFilePath)))
 			{
 				throw new DirectoryNotFoundException($"'{nameof(outputFilePath)}' must be in a directory that exists.");
 			}
 
-			var repositoryParser = new RepositoryParser.RepositoryParser();
-			var dependencyGrapher = new DependencyGrapher();
-			var projectBuilder = new ProjectBuilder(dependencyGrapher);
+			var projects = _RepositoryParser.ParseProjects(inputDirectory);
 
-			var projects = repositoryParser.ParseProjects(inputDirectory);
-			var solutions = repositoryParser.ParseSolutions(inputDirectory, projects);
-
-			var buildProject = projectBuilder.BuildBuildProject(projects);
-			var rawBuildProject = buildProject.ToString();
+			var buildProject = _ProjectBuilder.BuildBuildProject(projects);
 			buildProject.Save(outputFilePath);
 		}
 	}
